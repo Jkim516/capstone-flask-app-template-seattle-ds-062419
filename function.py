f0d75b51912ded1a5f4bee910629dcdb3c678afa
Jkim516 <Jungmokim516@gmail.com>
2019-10-02 16:52:59 -0700

    Updating files and edited text

--- function.py
+++ function.py
@@ -23,30 +23,44 @@ def get_live_station(url):
 
 
 def normalize_demand_number(record):
-    if record['y_hat'] >= 4.5:
-        return round(record['y_hat']) * 2.5
-    elif record['y_hat'] >= 4:
-        return round(record['y_hat']) * 2
+    if record['y_hat'] >= 4.3:
+        return round(record['y_hat']) * 3
+    elif record['y_hat'] >= 3.5:
+        return round(record['y_hat']) * 3
     elif record['y_hat'] >= 3:
         return round(record['y_hat']) * 2
     elif record['y_hat'] >= 1:
         return round(record['y_hat']) * 3
-    elif record['y_hat'] >= 0.7:
+    elif record['y_hat'] >= 0.5:
         return round(record['y_hat']) * 2
-    elif record['y_hat'] >= 0.4:
-        return round(record['y_hat']) * 1
+    elif record['y_hat'] >= 0.2:
+        return 1
+    else:
+        return 0
+
+def normalize_ys_number(record):
+    if record['ys'] >= 3:
+        return round(record['ys']) * 2
+    elif record['ys'] >= 1.5:
+        return round(record['ys']) * 3
+    elif record['ys'] >= 1:
+        return round(record['ys']) * 2
+    elif record['ys'] >= .7:
+        return round(record['ys']) * 1
     else:
         return 0
 
 def nexthour(record):
-    if record['bikesAvailable'] - record['demand'] >= 0:
-        return record['bikesAvailable'] - record['demand']
+    if record['bikesAvailable'] - record['demand'] + record['surplus'] >= 0:
+        return record['bikesAvailable'] - record['demand'] + record['surplus']
     else:
         return 0
 
 def make_map(station):
     with open('station_pred_df1.pickle', 'rb') as f:
         station_pred_df1 = pickle.load(f)
+    with open('end_station_pred_df.pickle', 'rb') as f:
+        end_station_pred_df = pickle.load(f)
 
     live_station_df = get_live_station("https://bikeshare.metro.net/stations/json/")
 
@@ -69,13 +83,22 @@ def make_map(station):
     live_station_df.reset_index(inplace=True)
     live_station_df['id'] = live_station_df.ds.astype(str) + '-' + live_station_df.kioskId
 
-    # Take first 4 character in station feature
+    # Take first 4 character in station feature in station_pred_df1 to create 'id' 
     station_pred_df1.station = station_pred_df1.station.str[:4]
     station_pred_df1['id'] = station_pred_df1.ds.astype(str) + '-' + station_pred_df1.station
     station_pred_df1.set_index('ds', inplace=True)
-
+    # Create feature call demand based on date & hour for each stations
     merge_live_pred_df = station_pred_df1.merge(live_station_df, on='id')
     merge_live_pred_df['demand'] = merge_live_pred_df.apply(normalize_demand_number, axis=1)
+    # end_station_pred_df 
+    end_station_pred_df.rename(columns={'y_hat': 'ys'}, inplace=True)
+    end_station_pred_df.station = end_station_pred_df.station.str[:4]
+    end_station_pred_df['id'] = end_station_pred_df.ds.astype(str) + '-' + end_station_pred_df.station
+    end_station_pred_df.set_index('ds', inplace=True)
+
+    merge_live_pred_df = merge_live_pred_df.merge(end_station_pred_df[['id', 'ys']], on='id')
+    merge_live_pred_df['surplus'] = merge_live_pred_df.apply(normalize_ys_number, axis=1)
+
     merge_live_pred_df['nextHour'] = merge_live_pred_df.apply(nexthour, axis=1)
 
 
